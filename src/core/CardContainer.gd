40353a0792c9ee7a9142969e53ce6f0d0db58697
Divided by Zer0 <mail@dbzer0.com>
2020-11-18 00:51:10 +0100

    standardizing cardcontainer

--- src/core/CardContainer.gd
+++ src/core/CardContainer.gd
@@ -1,9 +1,6 @@
-extends Area2D
 class_name CardContainer
+extends Area2D
 
-var waiting_for_card_drop: bool = false
-
-func get_class(): return "CardContainer"
 
 func _ready() -> void:
 	# warning-ignore:return_value_discarded
@@ -17,28 +14,6 @@ func _ready() -> void:
 			button.connect("mouse_exited",self,"_on_button_mouse_exited")
 	$Control/ManipulationButtons/Shuffle.connect("pressed",self,'_on_Shuffle_Button_pressed')
 
-func get_all_cards() -> Array:
-	# This function will return an array with all the hosted Card objects in this node.
-	var cardsArray := []
-	for obj in get_children():
-		# This comparison will return null if obj is not a Card class.
-		if obj as Card:
-			cardsArray.append(obj)
-	return cardsArray
-
-func get_card_count() -> int:
-	return len(get_all_cards())
-
-func get_card(idx: int) -> Card:
-	return get_all_cards()[idx]
-
-func get_card_index(card: Card) -> int:
-	return get_all_cards().find(card)
-
-func get_random_card() -> Card:
-	var cardsArray := get_all_cards()
-	randomize()
-	return cardsArray[randi()%len(cardsArray)]
 
 func _on_Control_mouse_entered() -> void:
 	# This function shows the container manipulation buttons when you hover over them
@@ -48,6 +23,7 @@ func _on_Control_mouse_entered() -> void:
 	Tween.TRANS_SINE, Tween.EASE_IN)
 	$Control/ManipulationButtons/Tween.start()
 
+
 func _on_Control_mouse_exited() -> void:
 	# This function hides the container manipulation buttons when you stop hovering over them
 	$Control/ManipulationButtons/Tween.remove_all() # We always make sure to clean tweening conflicts
@@ -56,16 +32,52 @@ func _on_Control_mouse_exited() -> void:
 	Tween.TRANS_SINE, Tween.EASE_IN)
 	$Control/ManipulationButtons/Tween.start()
 
+
 func _on_button_mouse_entered() -> void:
 	$Control/ManipulationButtons/Tween.remove_all()
 	$Control/ManipulationButtons.modulate[3] = 1
+
+
 func _on_button_mouse_exited():
 	$Control/ManipulationButtons/Tween.remove_all()
 	$Control/ManipulationButtons.modulate[3] = 0
 
+
 func _on_Shuffle_Button_pressed() -> void:
 	# Reshuffles the cards in container
 	shuffle_cards()
+
+
+func get_class(): return "CardContainer"
+
+
+func get_all_cards() -> Array:
+	# This function will return an array with all the hosted Card objects in this node.
+	var cardsArray := []
+	for obj in get_children():
+		# This comparison will return null if obj is not a Card class.
+		if obj as Card:
+			cardsArray.append(obj)
+	return cardsArray
+
+
+func get_card_count() -> int:
+	return len(get_all_cards())
+
+
+func get_card(idx: int) -> Card:
+	return get_all_cards()[idx]
+
+
+func get_card_index(card: Card) -> int:
+	return get_all_cards().find(card)
+
+
+func get_random_card() -> Card:
+	var cardsArray := get_all_cards()
+	randomize()
+	return cardsArray[randi()%len(cardsArray)]
+
 
 func shuffle_cards() -> void:
 	var cardsArray := []
@@ -75,6 +87,7 @@ func shuffle_cards() -> void:
 	cardsArray.shuffle()
 	for card in cardsArray:
 		move_child(card,cardsArray.find(card))
+
 
 func translate_card_index_to_node_index(index: int) -> int:
 	# This function is responsible for translating requested card index to true node index.
