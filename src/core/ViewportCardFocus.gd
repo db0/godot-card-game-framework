b192193d882b10e8ac8280f53ca5fbd6e9afcff4
Divided by Zer0 <mail@dbzer0.com>
2020-11-19 00:51:38 +0100

    converted to ViewportCardFocus vars to private vars

--- src/core/ViewportCardFocus.gd
+++ src/core/ViewportCardFocus.gd
@@ -5,12 +5,12 @@ class_name ViewportCardFocus
 extends Node2D
 
 # This array holds all the previously focused cards.
-var previously_focused_cards := []
+var _previously_focused_cards := []
 # This var hold the currently focused card duplicate.
-var current_focus_source : Card = null
+var _current_focus_source : Card = null
 # This ductionary holds the source origin for each dupe.
 # We use this during cleanup
-var dupes_dict := {}
+var _dupes_dict := {}
 
 
 # Called when the node enters the scene tree for the first time.
@@ -26,14 +26,14 @@ func _process(_delta) -> void:
 	else:
 		$Focus.rect_position.x = 0
 	# The below performs some garbage collection on previously focused cards.
-	for c in previously_focused_cards:
+	for c in _previously_focused_cards:
 		# We only delete old dupes if there's no tweening currently ongoing.
 		# This is to allow the fade-to-alpha to complete nicely when we
 		# unfocus a card.
 		# It does create a small glitch when quickly changing card focus.
 		# Haven't found a good way to avoid it yet
-		if current_focus_source != dupes_dict[c] and not $Focus/Tween.is_active():
-			previously_focused_cards.erase(c)
+		if _current_focus_source != _dupes_dict[c] and not $Focus/Tween.is_active():
+			_previously_focused_cards.erase(c)
 			c.queue_free()
 
 
@@ -41,21 +41,21 @@ func _process(_delta) -> void:
 func focus_card(card: Card) -> void:
 	# We check if we're already focused on this card, to avoid making duplicates
 	# the whole time
-	if not current_focus_source:
+	if not _current_focus_source:
 		# We make a duplicate of the card to display and add it on its own in
 		# our viewport world
 		# This way we can standardize its scale and look and not worry about
 		# what happens on the table.
 		var dupe_focus = card.duplicate()
-		current_focus_source = card
-		dupes_dict[dupe_focus] = card
+		_current_focus_source = card
+		_dupes_dict[dupe_focus] = card
 		# We hide the manipulation buttons if the card was cloned with them visible
 		dupe_focus.get_node('Control/ManipulationButtons').modulate[3] = 0
 		dupe_focus.get_node('Control').rect_rotation = 0
 		dupe_focus.complete_targeting()
 		# We store all our previously focused cards in an array, and clean them
 		# up when they're not focused anymore
-		previously_focused_cards.append(dupe_focus)
+		_previously_focused_cards.append(dupe_focus)
 		$Focus/Viewport.add_child(dupe_focus)
 		# We scale it up to allow the player a better viewing experience
 		dupe_focus.scale = Vector2(1.5,1.5)
@@ -71,8 +71,8 @@ func focus_card(card: Card) -> void:
 
 # Hides the focus viewport when we're done looking at it
 func unfocus(card: Card) -> void:
-	if current_focus_source == card:
-		current_focus_source = null
+	if _current_focus_source == card:
+		_current_focus_source = null
 		$Focus/Tween.remove_all()
 		$Focus/Tween.interpolate_property($Focus,'modulate',
 				$Focus.modulate, Color(1,1,1,0), 0.25,
