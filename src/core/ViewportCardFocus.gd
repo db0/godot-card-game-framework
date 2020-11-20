9f366a529e8fc8a23ec47048f84bd6c652e95c60
Divided by Zer0 <mail@dbzer0.com>
2020-11-20 23:59:11 +0100

    new card back

--- src/core/ViewportCardFocus.gd
+++ src/core/ViewportCardFocus.gd
@@ -53,11 +53,17 @@ func focus_card(card: Card) -> void:
 		dupe_focus.get_node('Control/ManipulationButtons').modulate[3] = 0
 		dupe_focus.get_node('Control').rect_rotation = 0
 		dupe_focus.complete_targeting()
-		# If the card has already been been viewed while down, 
+		# If the card has already been been viewed while down,
 		# we allow the player hovering over it to see it
-		if not dupe_focus.is_faceup and dupe_focus.is_viewed:
-			dupe_focus._flip_card(dupe_focus.get_node("Control/Back"), 
-					dupe_focus.get_node("Control/Front"), true)
+		if not dupe_focus.is_faceup:
+			if dupe_focus.is_viewed:
+				dupe_focus._flip_card(dupe_focus.get_node("Control/Back"),
+						dupe_focus.get_node("Control/Front"), true)
+			else:
+				# We slightly reduce the intensity of the dupe
+				# As its enlarged state makes it glow too much
+				var current_colour = dupe_focus.get_node('Control/Back').modulate
+				dupe_focus.get_node('Control/Back').modulate = current_colour * 0.95
 		# We store all our previously focused cards in an array, and clean them
 		# up when they're not focused anymore
 		_previously_focused_cards.append(dupe_focus)
