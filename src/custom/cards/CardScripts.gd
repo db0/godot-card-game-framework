caa31697270e630167565aee6abe03cb30d627e4
Divided by Zer0 <mail@dbzer0.com>
2020-11-25 18:55:12 +0100

    contributing

---
+++ src/custom/cards/CardScripts.gd
@@ -0,0 +1,10 @@
+class_name CardScripts
+extends Reference
+
+
+func get_scripts(card_name) -> Dictionary:
+	var scripts := {
+		"Test1" : [{'name':'rotate_self','args':["self",90]},
+			{'name':'move_self_to_container','args':["self",cfc.NMAP.deck]}]
+	}
+	return(scripts.get(card_name,[]))
