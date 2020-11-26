fffc35db326c914ab61a224f5b8049beed2d19ce
Divided by Zer0 <mail@dbzer0.com>
2020-11-26 01:51:50 +0100

    test cards have ability explanations

--- src/custom/cards/CardScripts.gd
+++ src/custom/cards/CardScripts.gd
@@ -4,16 +4,16 @@ extends Reference
 func get_scripts(card_name) -> Dictionary:
 	var scripts := {
 		"Test Card 1": {
-			"hand": [
-				{
-					'name': 'rotate_self',
-					'args': [90]
-				}
-			],
 			"board": [
 				{
+					'name': 'rotate_self',
+					'args': [90],
+				}
+			],
+			"hand": [
+				{
 					'name': 'move_self_to_container',
-					'args': [cfc.NMAP.deck]
+					'args': [cfc.NMAP.deck],
 				}
 			]
 		},
@@ -22,17 +22,17 @@ func get_scripts(card_name) -> Dictionary:
 			"board": [
 				{
 					'name': 'move_target_to_container',
-					'args': [cfc.NMAP.discard, false]
+					'args': [cfc.NMAP.discard, false],
 				},
 				{
 					'name': 'move_self_to_container',
-					'args': [cfc.NMAP.discard]
+					'args': [cfc.NMAP.discard],
 				}
 			],
 			"pile": [
 				{
 					'name': 'move_self_to_container',
-					'args': [cfc.NMAP.discard]
+					'args': [cfc.NMAP.discard],
 				}
 			]
 		},
@@ -41,17 +41,17 @@ func get_scripts(card_name) -> Dictionary:
 			"board": [
 				{
 					'name': 'flip_target',
-					'args': [false,true]
+					'args': [false,true],
 				},
 				{
 					'name': 'rotate_target',
-					'args': [180, true]
+					'args': [180, true],
 				}
 			],
 			"hand": [
 				{
 					'name': 'move_self_to_container',
-					'args': [cfc.NMAP.discard]
+					'args': [cfc.NMAP.discard],
 				}
 			]
 		},
