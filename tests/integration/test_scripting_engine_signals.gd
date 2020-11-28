73f7b455f13bf8436c106465e57bf616741e9431
Divided by Zer0 <mail@dbzer0.com>
2020-11-28 18:49:47 +0100

    IT for signal scripts

--- tests/integration/test_scripting_engine_signals.gd
+++ tests/integration/test_scripting_engine_signals.gd
@@ -22,6 +22,31 @@ func test_signals():
 	for sgn in  cfc.signal_propagator.known_card_signals:
 		assert_connected(card, cfc.signal_propagator, sgn, "_on_Card_signal_received")
 		assert_connected(target, cfc.signal_propagator, sgn, "_on_Card_signal_received")
+	watch_signals(target)
+	watch_signals(card)
+	# Test "self" trigger works
+	card.scripts = {"card_rotated": { "board": [
+			{"name": "flip_card",
+			"subject": "self",
+			"trigger": "self",
+			"set_faceup": false}]}}
+	yield(table_move(card, Vector2(100,100)), "completed")
+	card.card_rotation = 90
+	yield(yield_to(card._flip_tween, "tween_all_completed", 1), YIELD)
+	assert_signal_emitted_with_parameters(card,"card_flipped",[card,"card_flipped",[false]])
+	assert_signal_emitted_with_parameters(card,"card_rotated",[card,"card_rotated",[90]])
+	# Test "any" trigger works
+	target.scripts = {"card_rotated": { "board": [
+			{"name": "flip_card",
+			"subject": "self",
+			"set_faceup": false}]}}
+	yield(table_move(target, Vector2(500,100)), "completed")
+	target.card_rotation = 90
+	yield(yield_to(target._flip_tween, "tween_all_completed", 1), YIELD)
+	assert_signal_emitted_with_parameters(target,"card_flipped",[target,"card_flipped",[false]])
+	assert_signal_emitted_with_parameters(target,"card_rotated",[target,"card_rotated",[90]])
+
+func test_card_rotated():
 	watch_signals(card)
 	watch_signals(target)
 	card.scripts = {"card_rotated": { "board": [
@@ -33,5 +58,19 @@ func test_signals():
 	yield(table_move(target, Vector2(500,100)), "completed")
 	target.card_rotation = 90
 	yield(yield_to(target._tween, "tween_all_completed", 1), YIELD)
-	assert_signal_emitted(target,"card_rotated")
-	assert_signal_emitted(card,"card_rotated")
+	assert_signal_emitted_with_parameters(target,"card_rotated",[target,"card_rotated",[90]])
+	assert_signal_emitted_with_parameters(card,"card_rotated",[card,"card_rotated",[270]])
+
+
+func test_card_flipped():
+	watch_signals(card)
+	watch_signals(target)
+	card.scripts = {"card_flipped": { "hand": [
+			{"name": "flip_card",
+			"subject": "self",
+			"trigger": "another",
+			"set_faceup": false}]}}
+	target.is_faceup = false
+	yield(yield_to(target._flip_tween, "tween_all_completed", 1), YIELD)
+	assert_signal_emitted_with_parameters(target,"card_flipped",[target,"card_flipped",[false]])
+	assert_signal_emitted_with_parameters(card,"card_flipped",[card,"card_flipped",[false]])
