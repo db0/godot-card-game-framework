3567e027aac6ac1cb0785cb09735c3b9d88ec789
Divided by Zer0 <mail@dbzer0.com>
2020-12-06 15:59:44 +0100

    Tweaks to oval hand shape rotations
    Added tests for oval hand shape
    Fixed facedown tests

--- tests/integration/test_scaling_focus.gd
+++ tests/integration/test_scaling_focus.gd
@@ -7,14 +7,12 @@ func before_each():
 	cards = draw_test_cards(5)
 	yield(yield_for(1), YIELD)
 
-func test_single_card_focus():
+func test_single_card_focus_use_rectangle():
+	cfc.hand_use_oval_shape = false
 	cards[0]._on_Card_mouse_entered()
 	yield(yield_to(cards[0].get_node('Tween'), "tween_all_completed", 1), YIELD)
-	if cfc.hand_use_oval_shape:
-		pass
-	else:
-		assert_almost_eq(Vector2(47.5, -240),cards[0].position,Vector2(2,2),
-				"Card dragged in correct global position")
+	assert_almost_eq(Vector2(47.5, -240),cards[0].position,Vector2(2,2),
+			"Card dragged in correct global position")
 	assert_almost_eq(Vector2(1.5, 1.5),cards[0].scale,Vector2(0.1,0.1),
 			"Card has correct scale")
 	cards[0]._on_Card_mouse_exited()
@@ -23,23 +21,50 @@ func test_single_card_focus():
 			"Card placed in correct global position")
 	assert_almost_eq(Vector2(1, 1),cards[0].scale,Vector2(0.1,0.1),
 			"Card has correct scale")
-#
-func test_card_focus_neighbour_push():
+	cfc.hand_use_oval_shape = true
+
+func test_single_card_focus_use_oval():
+	cfc.hand_use_oval_shape = true
+	cards[0]._on_Card_mouse_entered()
+	yield(yield_to(cards[0].get_node('Tween'), "tween_all_completed", 1), YIELD)
+	assert_almost_eq(Vector2(104.116, -271.632),cards[0].position,Vector2(2,2),
+			"Card dragged in correct global position")
+	assert_almost_eq(Vector2(1.5, 1.5),cards[0].scale,Vector2(0.1,0.1),
+			"Card has correct scale")
+	cards[0]._on_Card_mouse_exited()
+	yield(yield_to(cards[0].get_node('Tween'), "tween_all_completed", 1), YIELD)
+	assert_almost_eq(cards[0].recalculate_position(),cards[0].position,Vector2(2,2),
+			"Card placed in correct global position")
+	assert_almost_eq(Vector2(1, 1),cards[0].scale,Vector2(0.1,0.1),
+			"Card has correct scale")
+	cfc.hand_use_oval_shape = true
+
+func test_card_focus_neighbour_push_use_rectangle():
+	cfc.hand_use_oval_shape = false
 	cards[2]._on_Card_mouse_entered()
 	yield(yield_for(1), YIELD)
-	if cfc.hand_use_oval_shape:
-		pass
-	else:
-		assert_almost_eq(Vector2(29, 0),cards[0].position,Vector2(2,2),
-				"Card dragged in correct global position")
-		assert_almost_eq(Vector2(137.5, 0),cards[1].position,Vector2(2,2),
-				"Card dragged in correct global position")
-		assert_almost_eq(Vector2(692.5, 0),cards[3].position,Vector2(2,2),
-				"Card dragged in correct global position")
-		assert_almost_eq(Vector2(801.25, 0),cards[4].position,Vector2(2,2),
-				"Card dragged in correct global position")
-
-
+	assert_almost_eq(Vector2(29, 0),cards[0].position,Vector2(2,2),
+			"Card dragged in correct global position")
+	assert_almost_eq(Vector2(137.5, 0),cards[1].position,Vector2(2,2),
+			"Card dragged in correct global position")
+	assert_almost_eq(Vector2(692.5, 0),cards[3].position,Vector2(2,2),
+			"Card dragged in correct global position")
+	assert_almost_eq(Vector2(801.25, 0),cards[4].position,Vector2(2,2),
+			"Card dragged in correct global position")
+	cfc.hand_use_oval_shape = true
+func test_card_focus_neighbour_push_use_oval():
+	cfc.hand_use_oval_shape = true
+	cards[2]._on_Card_mouse_entered()
+	yield(yield_for(1), YIELD)
+	assert_almost_eq(Vector2(102.718, -22.392),cards[0].position,Vector2(2,2),
+			"Card dragged in correct global position")
+	assert_almost_eq(Vector2(181.942, -39.661),cards[1].position,Vector2(2,2),
+			"Card dragged in correct global position")
+	assert_almost_eq(Vector2(648.058, -39.661),cards[3].position,Vector2(2,2),
+			"Card dragged in correct global position")
+	assert_almost_eq(Vector2(727.282, -22.392),cards[4].position,Vector2(2,2),
+			"Card dragged in correct global position")
+	cfc.hand_use_oval_shape = true
 func test_card_change_focus_to_neighbour():
 	var YIELD_TIME := 0.07
 	var YIELD_TIME2 := 0.5
