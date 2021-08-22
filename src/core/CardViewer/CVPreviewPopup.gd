a14c900d039be82208d74c421f0814c49c2c35b2
Divided by Zer0 <mail@dbzer0.com>
2021-08-22 13:01:12 +0200

    preview popups won't queue clear the card every time:

--- src/core/CardViewer/CVPreviewPopup.gd
+++ src/core/CardViewer/CVPreviewPopup.gd
@@ -57,20 +57,19 @@ func get_preview_placement() -> Vector2:
 # Instances a card object. Populates it with the card details
 # Then displays it in the popup.
 func show_preview_card(card_name) -> void:
-	preview_card = cfc.instance_card(card_name)
-	add_child(preview_card)
-	# It's necessary we do this here because if we only we it during
-	# the process, the card will appear to teleport
-	if CFConst.VIEWPORT_FOCUS_ZOOM_TYPE == "resize":
-		preview_card.resize_recursively(preview_card._control, CFConst.PREVIEW_SCALE)
-		preview_card.card_front.scale_to(CFConst.PREVIEW_SCALE)
-	rect_position = get_preview_placement()
-	cfc.ov_utils.populate_info_panels(preview_card,focus_info)
-	mouse_filter = Control.MOUSE_FILTER_IGNORE
+	if not is_instance_valid(preview_card):
+		preview_card = cfc.instance_card(card_name)
+		add_child(preview_card)
+		# It's necessary we do this here because if we only we it during
+		# the process, the card will appear to teleport
+		if CFConst.VIEWPORT_FOCUS_ZOOM_TYPE == "resize":
+			preview_card.resize_recursively(preview_card._control, CFConst.PREVIEW_SCALE)
+			preview_card.card_front.scale_to(CFConst.PREVIEW_SCALE)
+		rect_position = get_preview_placement()
+		cfc.ov_utils.populate_info_panels(preview_card,focus_info)
+		mouse_filter = Control.MOUSE_FILTER_IGNORE
 	visible = true
 
 # Deinstances the currently shown card.
 func hide_preview_card() -> void:
-	if is_instance_valid(preview_card):
-		preview_card.queue_free()
 	visible = false
